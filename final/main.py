e687a4ff8bd4796e0ddc26351988f42d25715e3e
Luis Kunz <luiskunzc@gmail.com>
2025-05-05 01:56:25 -0300

    feat: backend to frontend connection

--- final/main.py
+++ final/main.py
@@ -2,35 +2,42 @@ from funciones import GeneradorAleatorio
 
 
 def simular(self):
-    # Obtener valores de las tablas
     valores_tabla1 = [self.table1.item(fila, 0).text() for fila in range(self.table1.rowCount())]
     valores_tabla2 = [self.table2.item(fila, 0).text() for fila in range(self.table2.rowCount())]
     valores_tabla3 = [(self.table3.item(fila, 0).text(), self.table3.item(fila, 1).text()) for fila in
                       range(self.table3.rowCount())]
     valores_tabla4 = [(self.table4.item(fila, 0).text(), self.table4.item(fila, 1).text()) for fila in
                       range(self.table4.rowCount())]
-
-    # Obtener valores de los QDoubleSpinBox
     cantidad_semanas = self.inputCantidadSimulaciones.value()
     fila_inicial = self.inputFila.value()
     numero_filas = self.inputRangoFilas.value()
 
-    # Mostrar los valores en un cuadro de mensaje
-    mensaje = (
-        f"Tabla 1 (Pr. venta de autos): {valores_tabla1}\n"
-        f"Tabla 2 (Tipos de autos y probabilidades): {valores_tabla2}\n"
-        f"Tabla 3 (Comisiones y probabilidades): {valores_tabla3}\n"
-        f"Tabla 4 (Comisiones y probabilidades): {valores_tabla4}\n"
-        f"Cantidad de semanas (n): {cantidad_semanas}\n"
-        f"Filia inicial: {fila_inicial}\n"
-        f"numero de filas: {numero_filas}"
-    )
+    #TODO: Descomentar para ver los valores
+    # mensaje = (
+    #     f"Tabla 1 (Pr. venta de autos): {valores_tabla1}\n"
+    #     f"Tabla 2 (Tipos de autos y probabilidades): {valores_tabla2}\n"
+    #     f"Tabla 3 (Comisiones y probabilidades): {valores_tabla3}\n"
+    #     f"Tabla 4 (Comisiones y probabilidades): {valores_tabla4}\n"
+    #     f"Cantidad de semanas (n): {cantidad_semanas}\n"
+    #     f"Filia inicial: {fila_inicial}\n"
+    #     f"numero de filas: {numero_filas}"
+    # )
+    # print(mensaje)
 
-    print(mensaje)  # Para depuración, puedes eliminarlo si no es necesario
-    self.mostrar_exito(mensaje)
+    aleatorio4 = GeneradorAleatorio(19)
+    aleatorio3 = GeneradorAleatorio(20)
+    aleatorio2 = GeneradorAleatorio(21)
+    aleatorio1 = GeneradorAleatorio(22)
+
+    resultado = []
 
     for i in range(int(cantidad_semanas)):
-        print(i)
+        resultado.append(simular_semana(valores_tabla1, valores_tabla2, valores_tabla3, valores_tabla4, aleatorio1,
+                                        aleatorio2, aleatorio3, aleatorio4, i + 1))
+
+    print(resultado)
+
+    return mostrar_rango(int(fila_inicial), int(numero_filas), resultado)
 
 
 def simular_semana(t1, t2, t3, t4, a1, a2, a3, a4, i):
@@ -49,7 +56,8 @@ def simular_semana(t1, t2, t3, t4, a1, a2, a3, a4, i):
     ventas_all = [ventas_v1, ventas_v2, ventas_v3, ventas_v4]
 
     resultado = [i,
-        [rnd_ventas_v1, ventas_v1, rnd_ventas_v2, ventas_v2, rnd_ventas_v3, ventas_v3, rnd_ventas_v4, ventas_v4]]
+                 [rnd_ventas_v1, ventas_v1, rnd_ventas_v2, ventas_v2, rnd_ventas_v3, ventas_v3, rnd_ventas_v4,
+                  ventas_v4]]
 
     for i in range(len(ventas_all)):
         for j in range(ventas_all[i]):
@@ -62,13 +70,14 @@ def simular_semana(t1, t2, t3, t4, a1, a2, a3, a4, i):
             else:
                 resultado += [["-", "-", "-", "-"]]
 
-    #
+    #TODO: Agregar a la tabla resultado
     # resultado 2 a 5 de indice (1 a 4) tiene la ventas de v1 resultado[1][3] + [2][3] + [3][3] + [4][3] = comision semanal
     # resultado 2 a 5 de indice (1 a 4) tiene la ventas de v2 [5][3] + [6][3] + [7][3] + [8][3] = comision semanal
     # resultado 2 a 5 de indice (1 a 4) tiene la ventas de v3 [9][3] + [10][3] + [11][3] + [12][3] = comision semanal
     # resultado 2 a 5 de indice (1 a 4) tiene la ventas de v4 [13][3] + [14][3] + [15][3] + [16][3] = comision semanal
 
     return resultado
+
 
 def obtener_venta(t2, t3, t4, a2, a3, a4):
     rnd_tipo_auto = a2.generar_aleatorio()
@@ -111,27 +120,25 @@ def get_comision_auto(tablaComisiones, rnd):
 
 
 def mostrar_rango(inicial, numero_filas, resultado):
-    filas = resultado[inicial:inicial + numero_filas]
+    filas = resultado[inicial - 1:inicial - 1 + numero_filas]
     for i in range(len(filas)):
         print(filas[i])
     return filas
 
-
-if __name__ == '__main__':
-    aleatorio4 = GeneradorAleatorio(19)
-    aleatorio3 = GeneradorAleatorio(20)
-    aleatorio2 = GeneradorAleatorio(21)
-    aleatorio1 = GeneradorAleatorio(22)
-
-    resultado = []
-
-    for i in range(100000):
-        resultado.append(simular_semana(['0.2', '0.3', '0.3', '0.15', '0.05'], ['0.5', '0.35', '0.15'],
-                                        [('0.4', '400'), ('0.6', '500')],
-                                        [('0.35', '1000'), ('0.4', '1500'), ('0.25', '2000')], aleatorio1, aleatorio2,
-                                        aleatorio3,
-                                        aleatorio4,i+1))
-    # print("final")
-    # print(resultado)
-
-    mostrar_rango(22, 68, resultado)
+# TODO: Descomentar para probar backend
+# if __name__ == '__main__':
+#     print("Simulación iniciada")
+# aleatorio4 = GeneradorAleatorio(19)
+# aleatorio3 = GeneradorAleatorio(20)
+# aleatorio2 = GeneradorAleatorio(21)
+# aleatorio1 = GeneradorAleatorio(22)
+# resultado = []
+# for i in range(100000):
+#     resultado.append(simular_semana(['0.2', '0.3', '0.3', '0.15', '0.05'], ['0.5', '0.35', '0.15'],
+#                                     [('0.4', '400'), ('0.6', '500')],
+#                                     [('0.35', '1000'), ('0.4', '1500'), ('0.25', '2000')], aleatorio1, aleatorio2,
+#                                     aleatorio3,
+#                                     aleatorio4,i+1))
+# # print("final")
+# print(resultado)
+# mostrar_rango(22, 68, resultado)
--- final/test1.py
+++ final/test1.py
@@ -1,11 +1,10 @@
 import sys
-from main import simular
+from app import simular
 from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtWidgets import (
-    QApplication, QMainWindow, QTableWidget, QTableWidgetItem, QPushButton, QMessageBox, QLabel, QComboBox
+    QApplication, QMainWindow, QTableWidget, QTableWidgetItem, QPushButton, QMessageBox, QLabel, QComboBox, QHeaderView
 )
 from PyQt5.QtCore import QRect
-
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -13,7 +12,6 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("Tablas de Probabilidades")
         self.setGeometry(100, 100, 1200, 600)  # Tamaño de la ventana
-
 
         # Tabla 1: Probabilidades de venta de autos
         self.table1 = QTableWidget(5, 1, self)  # 5 filas y 1 columna
@@ -56,7 +54,6 @@ class MainWindow(QMainWindow):
         self.table4.setGeometry(QRect(850, 200, 250, 144))  # Posición y tamaño (x, y, ancho, alto)
         self.table4.setColumnWidth(0, 100)
 
-
         comision = [1000, 1500, 2000]
         probabilidades = [0.35, 0.40, 0.25]
         for fila, probabilidad in enumerate(probabilidades):
@@ -67,9 +64,9 @@ class MainWindow(QMainWindow):
         self.label = QLabel("Cantidad de semanas (n):", self)
         self.label.setGeometry(QRect(50, 10, 150, 30))
         self.inputCantidadSimulaciones = QtWidgets.QDoubleSpinBox(self)
-        self.inputCantidadSimulaciones.setGeometry(QtCore.QRect(200, 10, 100, 25))  # Posición y tamaño (x, y, ancho, alto)
+        self.inputCantidadSimulaciones.setGeometry(QtCore.QRect(200, 10, 100, 25))  #Posición y tamaño (x, y, ancho, alto)
         self.inputCantidadSimulaciones.setDecimals(0)  # No permitir decimales
-        self.inputCantidadSimulaciones.setMinimum(1.0)  # Valor mínimo permitido (1)
+        self.inputCantidadSimulaciones.setMinimum(100)  # Valor mínimo permitido (1)
         self.inputCantidadSimulaciones.setMaximum(1000000000.0)
 
         self.label1 = QLabel("Mostar desde fila:", self)
@@ -94,6 +91,109 @@ class MainWindow(QMainWindow):
         self.simular_button.setGeometry(QRect(500, 450, 200, 50))  # Posición y tamaño (x, y, ancho, alto)
         self.simular_button.clicked.connect(self.simular)
 
+        headers = [
+            'Reloj\n(Sem)', 'RND\nV1', 'Ventas\nV1', 'RND\nV2', 'Ventas\nV2',
+            'RND\nV3', 'Ventas\nV3', 'RND\nV4', 'Ventas\nV4',
+            'RND\nTipo\nAuto\nventa 1 v1', 'Tipo\nAuto v1', 'RND\nComisión\nv1', 'Comisión\nv1',
+            'RND\nTipo\nAuto\nventa 2 v1', 'Tipo\nAuto v2', 'RND\nComisión\nv2', 'Comisión\nv2',
+            'RND\nTipo\nAuto\nventa 3 v1', 'Tipo\nAuto v3', 'RND\nComisión\nv3', 'Comisión\nv3',
+            'RND\nTipo\nAuto\nventa 4 v1', 'Tipo\nAuto v4', 'RND\nComisión\nv4', 'Comisión\nv4',
+            'RND\nTipo\nAuto\nventa 1 v2', 'Tipo\nAuto v1', 'RND\nComisión\nv1', 'Comisión\nv1',
+            'RND\nTipo\nAuto\nventa 2 v2', 'Tipo\nAuto v2', 'RND\nComisión\nv2', 'Comisión\nv2',
+            'RND\nTipo\nAuto\nventa 3 v2', 'Tipo\nAuto v3', 'RND\nComisión\nv3', 'Comisión\nv3',
+            'RND\nTipo\nAuto\nventa 4 v2', 'Tipo\nAuto v4', 'RND\nComisión\nv4', 'Comisión\nv4',
+            'RND\nTipo\nAuto\nventa 1 v3', 'Tipo\nAuto v1', 'RND\nComisión\nv1', 'Comisión\nv1',
+            'RND\nTipo\nAuto\nventa 2 v3', 'Tipo\nAuto v2', 'RND\nComisión\nv2', 'Comisión\nv2',
+            'RND\nTipo\nAuto\nventa 3 v3', 'Tipo\nAuto v3', 'RND\nComisión\nv3', 'Comisión\nv3',
+            'RND\nTipo\nAuto\nventa 4 v3', 'Tipo\nAuto v4', 'RND\nComisión\nv4', 'Comisión\nv4',
+            'RND\nTipo\nAuto\nventa 1 v4', 'Tipo\nAuto v1', 'RND\nComisión\nv1', 'Comisión\nv1',
+            'RND\nTipo\nAuto\nventa 2 v4', 'Tipo\nAuto v2', 'RND\nComisión\nv2', 'Comisión\nv2',
+            'RND\nTipo\nAuto\nventa 3 v4', 'Tipo\nAuto v3', 'RND\nComisión\nv3', 'Comisión\nv3',
+            'RND\nTipo\nAuto\nventa 4 v4', 'Tipo\nAuto v4', 'RND\nComisión\nv4', 'Comisión\nv4',
+        ]
+        self.tableRes = QTableWidget(0, len(headers), self)
+        self.tableRes.setHorizontalHeaderLabels(headers)
+        self.tableRes.setGeometry(QRect(0, 500, 1920,490))  # Posición y tamaño (x, y, ancho, alto)
+        self.tableRes.setColumnWidth(0, 40)
+        self.tableRes.setColumnWidth(1, 20)
+        self.tableRes.setColumnWidth(2, 50)
+        self.tableRes.setColumnWidth(3, 20)
+        self.tableRes.setColumnWidth(4, 50)
+        self.tableRes.setColumnWidth(5, 20)
+        self.tableRes.setColumnWidth(6, 50)
+        self.tableRes.setColumnWidth(7, 20)
+        self.tableRes.setColumnWidth(8, 50)
+        self.tableRes.setColumnWidth(8, 50)
+
+        self.tableRes.setColumnWidth(9, 90)
+        self.tableRes.setColumnWidth(10, 80)
+        self.tableRes.setColumnWidth(11, 70)
+        self.tableRes.setColumnWidth(12, 70)
+        self.tableRes.setColumnWidth(13, 90)
+        self.tableRes.setColumnWidth(14, 80)
+        self.tableRes.setColumnWidth(15, 70)
+        self.tableRes.setColumnWidth(16, 70)
+        self.tableRes.setColumnWidth(17, 90)
+        self.tableRes.setColumnWidth(18, 80)
+        self.tableRes.setColumnWidth(19, 70)
+        self.tableRes.setColumnWidth(20, 70)
+        self.tableRes.setColumnWidth(21, 90)
+        self.tableRes.setColumnWidth(22, 80)
+        self.tableRes.setColumnWidth(23, 70)
+        self.tableRes.setColumnWidth(24, 70)
+
+        self.tableRes.setColumnWidth(25, 90)
+        self.tableRes.setColumnWidth(26, 80)
+        self.tableRes.setColumnWidth(27, 70)
+        self.tableRes.setColumnWidth(28, 70)
+        self.tableRes.setColumnWidth(29, 90)
+        self.tableRes.setColumnWidth(30, 80)
+        self.tableRes.setColumnWidth(31, 70)
+        self.tableRes.setColumnWidth(32, 70)
+        self.tableRes.setColumnWidth(33, 90)
+        self.tableRes.setColumnWidth(34, 80)
+        self.tableRes.setColumnWidth(35, 70)
+        self.tableRes.setColumnWidth(36, 70)
+        self.tableRes.setColumnWidth(37, 90)
+        self.tableRes.setColumnWidth(38, 80)
+        self.tableRes.setColumnWidth(39, 70)
+        self.tableRes.setColumnWidth(40, 70)
+
+        self.tableRes.setColumnWidth(41, 90)
+        self.tableRes.setColumnWidth(42, 80)
+        self.tableRes.setColumnWidth(43, 70)
+        self.tableRes.setColumnWidth(44, 70)
+        self.tableRes.setColumnWidth(45, 90)
+        self.tableRes.setColumnWidth(46, 80)
+        self.tableRes.setColumnWidth(47, 70)
+        self.tableRes.setColumnWidth(48, 70)
+        self.tableRes.setColumnWidth(49, 90)
+        self.tableRes.setColumnWidth(50, 80)
+        self.tableRes.setColumnWidth(51, 70)
+        self.tableRes.setColumnWidth(52, 70)
+        self.tableRes.setColumnWidth(53, 90)
+        self.tableRes.setColumnWidth(54, 80)
+        self.tableRes.setColumnWidth(55, 70)
+        self.tableRes.setColumnWidth(56, 70)
+
+        self.tableRes.setColumnWidth(57, 90)
+        self.tableRes.setColumnWidth(58, 80)
+        self.tableRes.setColumnWidth(59, 70)
+        self.tableRes.setColumnWidth(60, 70)
+        self.tableRes.setColumnWidth(61, 90)
+        self.tableRes.setColumnWidth(62, 80)
+        self.tableRes.setColumnWidth(63, 70)
+        self.tableRes.setColumnWidth(64, 70)
+        self.tableRes.setColumnWidth(65, 90)
+        self.tableRes.setColumnWidth(66, 80)
+        self.tableRes.setColumnWidth(67, 70)
+        self.tableRes.setColumnWidth(68, 70)
+        self.tableRes.setColumnWidth(69, 90)
+        self.tableRes.setColumnWidth(70, 80)
+        self.tableRes.setColumnWidth(71, 70)
+        self.tableRes.setColumnWidth(72, 70)
+
+
     def simular(self):
         # Validar tabla 1
         if not self.validar_probabilidades(self.table1, 0, 1):
@@ -117,7 +217,9 @@ class MainWindow(QMainWindow):
 
         # Si todas las validaciones pasan
         self.mostrar_exito("Todas las validaciones se completaron con éxito.")
-        simular(self)
+        vec = simular(self)
+
+        self.cargar_tabla(self.tableRes, vec)
 
     def validar_probabilidades(self, table, columna, suma_esperada):
         """Validar que las probabilidades no sean negativas y sumen un valor esperado."""
@@ -159,6 +261,21 @@ class MainWindow(QMainWindow):
         msg.setText(mensaje)
         msg.exec_()
 
+    def flatten(self, nested_list):
+        flat_list = []
+        for item in nested_list:
+            if isinstance(item, list):
+                flat_list.extend(self.flatten(item))  # Recursively flatten nested lists
+            else:
+                flat_list.append(item)  # Append non-list items directly
+        return flat_list
+
+    def cargar_tabla(self, table, vector):
+        table.setRowCount(len(vector))  # Configurar el número de filas
+        for fila_idx, fila in enumerate(vector):
+            fila_aux = self.flatten(fila)
+            for col_idx, valor in enumerate(fila_aux):
+                table.setItem(fila_idx, col_idx, QTableWidgetItem(str(valor)))
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
